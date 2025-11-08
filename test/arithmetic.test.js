13100734bdb114b96eedfc2e5679e4186f9ae06e
Shereen Faruqi <shereenfaruqi15@gmail.com>
2025-11-08 13:21:09 +0000

    Tested Power Function

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -93,7 +93,48 @@ describe('Arithmetic', function () {
         });
     });
 
-// TODO: Challenge #1
+describe('Power', function () {
+    it('computes power with positive integers', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2&operand2=3')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 8 });
+                done();
+            });
+    });
+    it('computes power with zero exponent', function (done) {
+        request.get('/arithmetic?operation=power&operand1=42&operand2=0')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 1 });
+                done();
+            });
+    });
+    it('computes power with negative exponent', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2&operand2=-2')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 0.25 });
+                done();
+            });
+    });
+    it('computes power with decimal base', function (done) {
+        request.get('/arithmetic?operation=power&operand1=0.5&operand2=2')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 0.25 });
+                done();
+            });
+    });
+    it('computes power with exponential notation', function (done) {
+        request.get('/arithmetic?operation=power&operand1=2e1&operand2=2')
+            .expect(200)
+            .end(function (err, res) {
+                expect(res.body).to.eql({ result: 400 });
+                done();
+            });
+    });
+});
  
 
     describe('Multiplication', function () {
